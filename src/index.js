2bc77bfc4b2c14b6bbbe4825297bd7e7a93b20ea
Nadiia <nenadinweb@gmail.com>
2022-03-05 01:05:58 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -2,20 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+/*test text*/
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick = {() => this.props.onClick()}
+      >
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value = {this.state.squares[i]}
+        onClick = { () => this.handleClick(i) }
+      />
+    );
   }
 
   render() {
